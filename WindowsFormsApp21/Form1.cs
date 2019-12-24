9b8061fcafc5b929d791361762d36dbefb68dc2b
wldms111 <852387@naver.com>
2019-12-24 15:02:22 +0900

    dd

--- WindowsFormsApp21/Form1.cs
+++ WindowsFormsApp21/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp21
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "버튼클릭";
+        }
     }
 }
